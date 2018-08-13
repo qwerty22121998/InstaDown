938f029473d8df871514a024804ddfded14cd999
VuHK <qwerty22121998@gmail.com>
2018-08-13 20:16:26 +0700

    fixed #2

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,8 @@ const ARTICLE_CLASS = "._8Rm4L.M9sTE.L_LMM.SgTZ1"
 const IMG_CLASS = ".KL4Bh"
 const VID_CLASS = "._5wCQW"
 const BAR_CLASS = ".ltpMr.Slqrh"
-
+const LEFT_MOVE = ".coreSpriteLeftChevron"
+const RIGHT_MOVE = ".coreSpriteRightChevron"
 
 let getMediaUrl = dom => {
     let img = $(dom).find(IMG_CLASS)
@@ -19,11 +20,15 @@ let cvtFromTextToElement = text => {
     return doc.body.firstChild
 }
 
-let newButton = url => {
-    console.log(url)
+let sendDownMsg = dom => {
+    url = getMediaUrl(dom)
+    chrome.runtime.sendMessage(url)
+}
+
+let newButton = dom => {
     let button = cvtFromTextToElement('<button class="oF4XW dCJp8 isg"><span class="glyphsSpriteDirect__outline__24__grey_9" aria-label="Download this image/video"></span></button>')
     $(button).click(() => {
-        chrome.runtime.sendMessage(url)
+        sendDownMsg(dom)
     })
     return button
 }
@@ -32,17 +37,21 @@ let getButtonBar = dom => {
     return $(dom).find(BAR_CLASS)[0]
 }
 
+let getMoveButton = dom => {
+    return $.merge($(dom).find(LEFT_MOVE), $(dom).find(RIGHT_MOVE))
+}
+
 
 let addButtonToBar = (list, button) => {
     list.lastChild.before(button)
 }
 
-
 let addDownLoadButton = () => {
     $(ARTICLE_CLASS + ":not(:has(.isg))").each((index, element) => {
         let url = getMediaUrl(element)
-        if (url != null)
-            addButtonToBar(getButtonBar(element), newButton(url))
+        if (url != null) {
+            addButtonToBar(getButtonBar(element), newButton(element))
+        }
     })
 }
 
